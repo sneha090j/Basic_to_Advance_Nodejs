3328cf9e636d896689e5df68c3de3b7682b77bbf
sneha090j <sneha090j@gmail.com>
2024-07-18 20:01:54 +0530

    path module

---
+++ pathmodule/path.js
@@ -0,0 +1,4 @@
+const path=require('path');
+console.log(path.dirname("C:/Users/Sneha/Documents/webdev/backend/new/pathmodule/path.js"));
+console.log(path.extname("C:/Users/Sneha/Documents/webdev/backend/new/pathmodule/path.js"));
+console.log(path.parse("C:/Users/Sneha/Documents/webdev/backend/new/pathmodule/path.js").root);
